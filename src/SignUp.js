75cc39e3d9c9c6dcb2fe47edeac378d3689a1e06
vibhor-25 <vibhoriitb@gmail.com>
2025-01-19 13:20:13 +0530

    Add files via upload

---
+++ src/SignUp.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const SignUp = () => {
+
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    return (
+        <div className="sign-up">
+            <h2>Sign-Up</h2>
+            <form>
+                <input type="text"
+                    required
+                    placeholder="Name"
+                    value = {name}
+                    onChange={(e) => {setName(e.target.value)}}
+                />
+                <input type="text"
+                    required
+                    placeholder="Username"
+                    value = {username}
+                    onChange={(e) => {setUsername(e.target.value)}}
+                />
+                <input type="text"
+                    required
+                    placeholder="Password"
+                    value = {password}
+                    onChange={(e) => {setPassword(e.target.value)}}
+                />
+                <input type="text"
+                    required
+                    placeholder="Confirm Password"
+                    value = {confirmPassword}
+                    onChange={(e) => {setConfirmPassword(e.target.value)}}
+                />
+                <input type="text"
+                    required
+                    placeholder="Email"
+                    value = {email}
+                    onChange={(e) => {setEmail(e.target.value)}}
+                />
+
+                <button>Create Account</button>
+
+            </form>
+        </div> 
+     );
+}
+ 
+export default SignUp;
